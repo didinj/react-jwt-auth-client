a116e2df4598beb1923050dc174ec8d6b8ca509c
didinj <didinj@gmail.com>
2025-05-19 11:56:13 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./screens/Login";
+import Home from "./screens/Home";
+import RegisterForm from "./screens/RegisterForm";
+import "./App.css";
 
 function App() {
+  const [authenticated, setAuthenticated] = useState(
+    !!localStorage.getItem("token")
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/login"
+          element={<Login onLogin={() => setAuthenticated(true)} />}
+        />
+        <Route
+          path="/"
+          element={authenticated ? <Home /> : <Navigate to="/login" />}
+        />
+        <Route path="/register" element={<RegisterForm />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
